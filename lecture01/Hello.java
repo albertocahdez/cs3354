9a78bdd2cc82363cc861a894931a9ca635009626
Alberto Cahdez <gxy16@txstate.edu>
2026-01-22 01:08:09 -0600

    lecture01 and lecture02 files updates for spring 2026 semester.

---
+++ lecture01/Hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("My first Java program.");
+		System.out.print("CS");
+		System.out.print(" 3354");
+		System.out.print(" Object-Oriented Design and Programming.\n");
+	}
+}
