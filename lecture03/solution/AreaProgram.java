f221adf12ff72f75ffea6d17c96a5cf9b32d6982
Alberto Cahdez <gxy16@txstate.edu>
2026-01-29 06:46:53 -0600

    lecture03 reorganized (reduced). The rest of the content was moved to lecture04.

--- lecture03/solution/AreaProgram.java
+++ lecture03/solution/AreaProgram.java
@@ -12,6 +12,7 @@ public class AreaProgram {
 	public static void main(String[] args) {
 		Scanner keyboard = new Scanner(System.in);
 		int option;
+		double area;
 
 		do {
 			System.out
@@ -24,54 +25,28 @@ public class AreaProgram {
 			else
 				switch (option) {
 					case 1:
-						showSquareDialog(keyboard);
+						System.out.print("--- Area of a Square ---\nEnter the square's lenght: ");
+						double length = keyboard.nextDouble();
+						area = Math.pow(length, 2);
+						System.out.println("Square's area is: " + area);
 						break;
 					case 2:
-						showCircleDialog(keyboard);
+						System.out.print("--- Area of a Circle ---\nEnter the cirle's radius: ");
+						double radius = keyboard.nextDouble();
+						area = Math.pow(radius, 2) * Math.PI;
+						System.out.println("Circle's area is: " + area);
 						break;
 					case 3:
-						showTriangleDialog(keyboard);
+						System.out.print("--- Area of a Triangle ---\nEnter the triangle's base: ");
+						double base = keyboard.nextDouble();
+						System.out.print(
+								"--- Area of a Triangle ---\nEnter the triangle's height measure: ");
+						double height = keyboard.nextDouble();
+						area = base * height / 2.0;
+						System.out.println("Triangles's area is: " + area);
 						break;
 				}
 		} while (option != 4);
 		keyboard.close();
-	}
-
-	/**
-	 * Displays a dialog to request square's length and compute its area.
-	 * 
-	 * @param input a Scanner object to read user input
-	 */
-	public static void showSquareDialog(Scanner input) {
-		System.out.print("--- Area of a Square ---\nEnter the square's lenght: ");
-		double length = input.nextDouble();
-		double area = Math.pow(length, 2);
-		System.out.println("Square's area is: " + area);
-	}
-
-	/**
-	 * Displays a dialog to request circle's radius and compute its area.
-	 * 
-	 * @param input a Scanner object to read user input
-	 */
-	public static void showCircleDialog(Scanner input) {
-		System.out.print("--- Area of a Circle ---\nEnter the cirle's radius: ");
-		double radius = input.nextDouble();
-		double area = Math.pow(radius, 2) * Math.PI;
-		System.out.println("Circle's area is: " + area);
-	}
-
-	/**
-	 * Displays a dialog to request triangle's base and height and compute its area.
-	 * 
-	 * @param input a Scanner object to read user input
-	 */
-	public static void showTriangleDialog(Scanner input) {
-		System.out.print("--- Area of a Triangle ---\nEnter the triangle's base: ");
-		double base = input.nextDouble();
-		System.out.print("--- Area of a Triangle ---\nEnter the triangle's height measure: ");
-		double height = input.nextDouble();
-		double area = base * height / 2.0;
-		System.out.println("Triangles's area is: " + area);
 	}
 }
